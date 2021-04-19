f1e0d46e050c2f82c6ae5606a9ff00ab951c9f14
LeaCrazyBambi <lheynitz@gmail.com>
2021-04-19 18:19:03 +0200

    I am missing 3.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+const myArray = ["Flower", "Spring", "Park", "Friends", "Drinks"];
+console.log(myArray);
+
+const thirdElement = myArray.slice(2,3);
+console.log(thirdElement);
+
+words.splice(-2, 1, "fourth element");
+
+
